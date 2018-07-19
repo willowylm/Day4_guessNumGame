e8f4e2751d7e2724b3f34ff4928226532884e4e4
Liuming Yang <ylmap@126.com>
2018-07-19 22:39:43 +0800

    Task 1~4 have done

--- guessNum.js
+++ guessNum.js
@@ -1,16 +1,23 @@
+
 class guessNum{
     constructor(){
 
     }
 
     test(input){
-      let targetNum=[1,2,3,4];
+    //   let targetNum=[1,2,3,4];
+    for (let i=0;i<4;i++)
+    {
+     var randomNumber=~~(Math.random()*10);
+     targetNum.push(randomNumber);
+    }
+
       let inputNum=input.split(' ');
 
     //   let xNUm = inputNum.filter((item,i)=>item==targetNum[i]).length
     //   return `${xNUm}A0B` ;
 
-    //   let yNUm = inputNum.filter((item,i)=>item != targetNum[i] && inputNum.includes(item)).length
+    //   let yNUm = inputNum.filer((item,i)=>item != targetNum[i] && inputNum.includes(item)).length
       const set = new Set();
       targetNum.forEach(number => set.add(number));
       let yNUm = 0;
--- guessNum.test.js
+++ guessNum.test.js
@@ -16,5 +16,16 @@ describe('guessNum', function () {
   it('should return 0A2B for input 0 3 2 4 when answer is 1 2 3 4 ', function () {
     expect(new guessNum().test('0 3 2 4')).toEqual('1A2B');
   });
+
+  it('should return 0A2B for input 5 6 7 8 when answer is 1 2 3 4 ', function () {
+    expect(new guessNum().test('5 6 7 8')).toEqual('0A0B');
+  });
+
+  it('should return 0A2B for input 5 6 7 8 when answer is 1 2 3 4 ', function () {
+    let random = 
+    expect(new guessNum().test('5 6 7 8')).toEqual('0A0B');
+  });
+
+  
 });
 
